431cbf30ab9a3c198a7621b630056a3884b8467d
Paulina López Salinas <marselluswallace@Marsellus-Wallace.local>
2020-07-15 16:21:47 -0500

    shopping crap

---
+++ public/js/customer.js
@@ -0,0 +1,8 @@
+
+    $(".btnshopping").on("click", function() {
+        console.log("click")
+        $(".shopping-cart").fadeToggle( "fast");
+    });
+   
+    
+    console.log($(".shopping-right"))
